8b7b1fbf52e306fe351a45bd0a68009edc79a7f6
OKUNOKENTARO <okunokentaro@me.com>
2021-07-13 14:54:49 +0900

    minimum 200

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,11 @@ import express from 'express';
 function main(): void {
   const app = express();
 
+  app.use((req, res, next)=> {
+    console.log(req, res, next);
+    res.status(200).end();
+  });
+
   const port = 3000;
   app.listen(port, () => console.log(`listening at http://localhost:${port}`));
 }
