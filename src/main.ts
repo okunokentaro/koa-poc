c0ae4490453b540a2d120f5a4618381e008da5bf
OKUNOKENTARO <okunokentaro@me.com>
2021-07-13 22:58:14 +0900

    example

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,30 @@
-import express from "express";
+import express, { ErrorRequestHandler } from "express";
 
 function main(): void {
   const app = express();
 
-  app.use(() => {
-    console.log("middleware 100");
-    throw new Error("middleware 110 Error");
+  app.use((_, __, next) => {
+    try {
+      console.log("middleware 100");
+      throw new Error("middleware 110 Error");
+    } catch (e) {
+      next(e);
+    }
   });
 
-  app.use((_, res) => {
-    console.log("middleware 200");
-    res.status(200).end();
-  });
+  app.use(
+    ((): ErrorRequestHandler => {
+      return (err, req, res, next) => {
+        console.log("middleware 200");
+        console.log("err", err);
+        console.log("err instanceof Error", err instanceof Error); // true
+        console.log("req", req);
+        console.log("res", res);
+        console.log("next", next);
+        res.status(500).end();
+      };
+    })()
+  );
 
   const port = 3000;
   app.listen(port, () => console.log(`listening at http://localhost:${port}`));
