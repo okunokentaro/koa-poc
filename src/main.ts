d15601fac890cc012ca1e806db1ad37f48c3d9c0
OKUNOKENTARO <okunokentaro@me.com>
2021-07-13 14:45:00 +0900

    express

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
-import Koa from 'koa';
+import express from 'express';
 
 function main(): void {
-  const app = new Koa();
+  const app = express();
 
   const port = 3000;
   app.listen(port, () => console.log(`listening at http://localhost:${port}`));
